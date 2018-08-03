a30560d376e31b222bacd071b34080159b0dcc29
jacobp <jacob.phillips@claimlogiq.com>
2018-08-03 09:45:09 -0500

    Editing UI and Page Navigation

--- WingtipToys/Global.asax.cs
+++ WingtipToys/Global.asax.cs
@@ -6,16 +6,21 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Data.Entity;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
-    public class Global : HttpApplication
-    {
-        void Application_Start(object sender, EventArgs e)
-        {
-            // Code that runs on application startup
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-    }
+	 public class Global : HttpApplication
+	 {
+		  void Application_Start(object sender, EventArgs e)
+		  {
+				// Code that runs on application startup
+				RouteConfig.RegisterRoutes(RouteTable.Routes);
+				BundleConfig.RegisterBundles(BundleTable.Bundles);
+			
+				//Initialize the product database
+				Database.SetInitializer(new ProductDatabaseInitializer());
+		  }
+	 }
 }
